01792494ae67b5a503866e755dc9a43c87b26488
MichaelWilliam Giannini <michaelwilliam.giann@mymail.champlain.edu>
2022-01-20 20:27:11 -0500

    Add header

--- l1/sort.py
+++ l1/sort.py
@@ -1,3 +1,20 @@
+#     Author: MW Giannini
+#     Class: CSI-275-01
+#     Assignment: Lab 1
+#     Date Assigned: Jan 18th 2022
+#     Due Date: Jan 24th 2022
+#     Description: A program to get a list of numbers from a user and sort it.
+#     I certify that this is entirely my own work, except where I have given
+#     fully-documented references to the work of others. I understand the definition and
+#     consequences of plagiarism and acknowledge that the assessor of this assignment
+#     may, for the purpose of assessing this assignment:
+#     - Reproduce this assignment and provide a copy to another member of academic staff;
+#     and/or
+#     - Communicate a copy of this assignment to a plagiarism checking service (which may
+#     then retain a copy of this assignment on its database for the purpose of future
+#     plagiarism checking)
+
+
 def build_list():
     keyword = "exit"
     numbers = []
